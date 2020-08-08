d11718606d35fe040f14f614f62130b20e2af13d
Sara A <calisaraalhindi@gmail.com>
2020-08-08 21:59:34 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,4 +1,5 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(hash_name)
+def key_for_min_value(name_hash)
+  
