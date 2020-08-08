f5be37ea9af61eea049fc3687edd03adb731d0c0
Sara A <calisaraalhindi@gmail.com>
2020-08-08 21:52:47 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,9 +2,5 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  name_hash.each do |name, num|
-    if num == 1
-      puts name
-    end
-end
+
 end
